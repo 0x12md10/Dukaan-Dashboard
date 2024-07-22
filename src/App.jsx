ba815ab393b375eae62051838da27b1a6e1322c5
0x12md10 <abish102001@gmail.com>
2024-07-22 23:16:30 +0530

    revenue card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@
 
 
 import './App.css'
+import RevenueCard from './components/RevenueCard/RevenueCard'
 
 function App() {
 
 
   return (
-    <div className='flex justify-center '>
-      Dukaan dashboard
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ml-10 mr-10 mt-10'>
+      <RevenueCard type="Next Payout" amount="2,312.23" orderCount={23} isPayout={true} dateNTime={"Today, 4:00PM"} />
+      <RevenueCard type="Amount Pending" amount="92,312.20" orderCount={13}/>
+      <RevenueCard type="Amount Processed" amount="23,91,221.20" orderCount={13}/>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        blue : {
+          500 :"#146EB4" ,
+          700 : "#0E4F82"
+        } 
+      }
+    },
   },
   plugins: [],
 }
